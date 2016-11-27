ec8027fdd5410710ad056b591656e52149426e3a
tapukoko <jupithanda@gmail.com>
2016-11-27 22:38:53 +0900

    161127 업데이트
    1.1

--- script_res/setdex_sm.js
+++ script_res/setdex_sm.js
@@ -9274,3 +9274,43 @@ var SETDEX_SM={
     }    
   }
 };
+
+var components = [
+    SETDEX_SHOWDOWN,
+    SETDEX_NUGGETBRIDGE,
+    SETDEX_CUSTOM
+];
+
+for (var i=0; i<components.length; i++) {
+    var sourceDex = components[i];
+    if (sourceDex) {
+        for (var p in sourceDex) {
+            if (sourceDex.hasOwnProperty(p)) {
+                SETDEX_XY[p] = $.extend(SETDEX_XY[p], sourceDex[p])
+                SETDEX_SM[p] = $.extend(SETDEX_SM[p], sourceDex[p])
+            }
+        }
+    }
+}
+
+var reloadSMScript = function()
+{
+    components = [
+    SETDEX_SHOWDOWN,
+    SETDEX_NUGGETBRIDGE,
+    SETDEX_CUSTOM
+];
+
+for (var i=0; i<components.length; i++) {
+    sourceDex = components[i];
+    if (sourceDex) {
+        for (var p in sourceDex) {
+            if (sourceDex.hasOwnProperty(p)) {
+                SETDEX_SM[p] = $.extend(SETDEX_SM[p], sourceDex[p])
+            }
+        }
+    }
+}
+}
+
+reloadSMScript();
