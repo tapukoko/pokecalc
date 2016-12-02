b592c73891f0e8b7956ab3c5c41299991b32a99f
tapukoko <jupithanda@gmail.com>
2016-12-03 02:07:26 +0900

    Update custom_help.js

--- script_res/custom_help.js
+++ script_res/custom_help.js
@@ -2,4 +2,3 @@ function custom_example(){
 	document.getElementById('spreadName').value = "스카프 에브이";
     document.getElementById('customMon').value = "에브이 @ 구애스카프 \n특성: 매직미러 \n레벨: 50 \n노력치: 52 H / 0 A / 0 B / 248 C / 0 D / 208 S \n성격: 조심 \n개체값: 31 H / 0 A / 31 B / 30 C / 31 D / 30 S \n- 사이코키네시스 \n- 매지컬샤인 \n- 잠재파워-불 \n- 트릭";
 }
-
