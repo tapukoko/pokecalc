839c4f49e51a8300c02ca5277cd6ca011d0deb1f
tapukoko <jupithanda@gmail.com>
2016-12-07 23:27:46 +0900

    Add files via upload

---
+++ script_res/faq_toggle.js
@@ -0,0 +1,16 @@
+$( "#faq1" ).click(function() {
+$( "#faq1Content" ).slideToggle();
+});
+
+$( "#faq2" ).click(function() {
+$( "#faq2Content" ).slideToggle();
+});
+
+$( "#faq3" ).click(function() {
+$( "#faq3Content" ).slideToggle();
+});
+
+$( "#faq4" ).click(function() {
+$( "#faq4Content" ).slideToggle();
+});
+
